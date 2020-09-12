f15c1d980d2b810828d01230033d0a1c864cbf9c
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-09-12 13:16:01 +0200

    example web

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import sciter
 import libs
 
 if __name__ == '__main__':
-    frame = sciter.Window(ismain=True, uni_theme=True)
-    frame.load_file("web/minimal.htm")
-    frame.expand()
-    frame.run_app()
+	frame = sciter.Window(ismain=True, uni_theme=True)
+	#example web, learning purposes
+	#frame.load_file("web/example.html")
+	frame.expand()
+	frame.run_app()
