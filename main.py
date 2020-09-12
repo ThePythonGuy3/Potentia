2cffdd2a1c3eb664d868ef63f96b97902a8efcb3
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-09-12 14:38:35 +0200

    stuff

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from libs.pnglib import *
 if __name__ == '__main__':
 	frame = sciter.Window(ismain=True, uni_theme=True)
 	#example web, learning purposes
-	frame.load_file("web/example.html")
+	frame.load_file("web/index.html")
 	frame.expand()
 	frame.run_app()
---
+++ push.py
@@ -0,0 +1,10 @@
+import os
+
+name = input("Commit name: ")
+
+os.system("git add .")
+os.system("git commit -m " + name)
+os.system("git push")
+
+while 1:
+	print(" ", end = "\r")
